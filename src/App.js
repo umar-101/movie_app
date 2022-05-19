1fd590f122bce93ad78e1c7c47658fdccc3e481e
UMAR <umarmushtaq101@gmail.com>
2022-05-19 21:55:17 +0500

    title and search bar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
 
-function App() {
+import "./App.css";
+import searchIcon from "./search.svg";
+
+const API_URL = "http://www.omdbapi.com/?apikey=91a32a88&";
+const App = () => {
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+
+    const data = await response.json();
+
+    console.log(data.Search);
+  };
+  useEffect(() => {
+    searchMovies("Downfall");
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Movies Counter</h1>
+      <div className="search">
+        <input
+          placeholder="Search for movies"
+          value="superman"
+          onChange={() => {}}
+        />
+        <img src={searchIcon} alt="search" onClick={() => {}} />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
